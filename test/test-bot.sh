5110cb6c1110f753c704bc7b1259ef75ccf7568b
Vitor <ntw1942@gmail.com>
2023-02-04 15:27:53 -0300

    fix: test script

--- test/test-bot.sh
+++ test/test-bot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GITPATH=/home/vitor/git/
+GITPATH=/home/vitor/git
 PROJECT=discord-bot-ntw
 
-GIT=${PROJECT}/${PROJECT}
+GIT=${GITPATH}/${PROJECT}
 
 CTNR_NAME=discord-bot
 
@@ -11,9 +11,9 @@ cd ${GIT}
 
 git pull
 
-docker build -t ${PROJECT}.
+docker build -t ${PROJECT} .
 
 docker stop ${PROJECT}
 docker rm ${PROJECT}
 
-docker run --restart=always --name ${CTNR_NAME} ${PROJECT}
+docker run -d --restart=always --name ${CTNR_NAME} ${PROJECT}
