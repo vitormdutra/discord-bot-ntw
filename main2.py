717abc97f86caf8de6631c597c9a64a59eb0664a
Vitor <ntw1942@gmail.com>
2023-02-22 22:57:54 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -22,7 +22,7 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/')
 
 @bot.event
 async def main():
-    async with bot:
+    with bot:
         await bot.add_cog(music_cog(bot))
         await bot.start('MTAyNjIzOTk1MzE2MTY4Mjk0NQ.G8QMtT.y1-RXvr2T2tW4Ug7hiTaKtYP9RkpmVZ2ZSGThs')
 
