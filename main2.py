621850383ee0133f2ce625bf4b7cc3eb94c58a11
Vitor <ntw1942@gmail.com>
2023-02-22 23:34:37 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -23,8 +23,8 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/')
 # bot.run("MTAyNjIzOTk1MzE2MTY4Mjk0NQ.G8QMtT.y1-RXvr2T2tW4Ug7hiTaKtYP9RkpmVZ2ZSGThs")
 
 async def main():
-        await bot.add_cog(music_cog(bot))
-        await bot.start('MTAyNjIzOTk1MzE2MTY4Mjk0NQ.G8QMtT.y1-RXvr2T2tW4Ug7hiTaKtYP9RkpmVZ2ZSGThs')
+    await bot.add_cog(music_cog(bot))
+    await bot.start('MTAyNjIzOTk1MzE2MTY4Mjk0NQ.G8QMtT.y1-RXvr2T2tW4Ug7hiTaKtYP9RkpmVZ2ZSGThs')
 
 
 asyncio.run(main())
