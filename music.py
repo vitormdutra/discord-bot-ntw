781074a1a0928be525e3e6685d4b0c8ffb6933ee
Vitor <ntw1942@gmail.com>
2023-02-27 21:29:11 -0300

    Update music.py

--- music.py
+++ music.py
@@ -78,8 +78,8 @@ class Music(commands.Cog):
     @commands.command()
     async def yt(self, ctx, *, url):
         """Plays from a url (almost anything youtube_dl supports)"""
-        channel = ctx.author.voice.channel.connect()
-        await channel.connect()
+
+        await ctx.author.voice.channel.connect()
 
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop)
