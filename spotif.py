89eda092c9fd3a0ef5f303ff760e32e8aab95a3d
Vitor <ntw1942@gmail.com>
2023-03-01 16:43:13 -0300

    Create spotif.py

---
+++ spotif.py
@@ -0,0 +1,18 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
+
+
+        #export SPOTIPY_CLIENT_ID='bf4f5f8fb16240e594f8bf440c848483'
+        #export SPOTIPY_CLIENT_SECRET='2db8ef640cc54512a9b8067873510495'
