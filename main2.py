780edcda8d112b4409784b0ecf797da197768a41
Vitor <ntw1942@gmail.com>
2023-03-01 17:28:09 -0300

    teste

--- main2.py
+++ main2.py
@@ -1,12 +1,7 @@
-import asyncio
-
 import discord
 from discord.ext import commands
-import os
 
 # import all of the cogs
-from help_cog import help_cog
-from music_cog import music_cog
 from music import Music
 
 # remove the default help command so that we can write out own
